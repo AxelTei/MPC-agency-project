c224b3f4bd1137e03d20538a0f2288621315265e
Axel Teisseire <mr.teisseire@gmail.com>
2022-12-19 17:19:54 +0100

    add button to the top to hygienes.html with addEventListener function on app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+let myButton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction()
+{
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+}
+
+function topFunction() 
+{
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+myButton.addEventListener("click", topFunction);
